1abf363a5c0fdb6a75d846e6f4333e26efcbdba8
sheetalskumar <sheetal.s.kumar@outlook.com>
2022-03-16 15:16:31 +1300

    Changed to App.js file

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,29 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
 
-function App = () =>
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      count: 0,
+    }
+  }
 
-  <div className="App">
-    <h1>Welcome to CISE - the home of learning and fun</h1>
-  </div>
+  makeIncrementer = amount => () =>
+    this.setState(prevState => ({
+      count: prevState.count + amount,
+    }));
+
+  increment = this.makeIncrementer(1);
+
+  render() {
+    return (
+      <div>
+        <p>Count: {this.state.count}</p>
+        <button className="increment" onClick={this.increment}>Increment count</button>
+      </div>
+    )
+  }
+}
 
 export default App;
+
