f2a9fe005c2a56a296a817c932476d147216dcfc
sheetalskumar <sheetal.s.kumar@outlook.com>
2022-03-16 14:59:49 +1300

    First Commit in CISE_ass1a_worksheet2

---
+++ backend/server.js
@@ -0,0 +1,27 @@
+const express = require("express");
+
+const articles = require("./dummydata/articles");
+
+const server = express();
+
+server.get('/', (req,res) => {
+    res.send("API is Running");
+})
+
+server.get('/api/articles', (req,res) => {
+    res.json(articles);
+})
+
+server.get('/api/articles/:id', (req,res) => {
+    const article = articles.find((n) => n._id === req.params.id);
+    res.send(article);
+    console.log(req.params);
+});
+
+const dotenv = require("dotenv");
+dotenv.config();
+const PORT = process.env.PORT || 5000;
+
+
+//server.listen(5000, console.log("Server is working and listening to PORT 5000"));
+server.listen(PORT, console.log(`server is working and listening on PORT ${PORT}`));
